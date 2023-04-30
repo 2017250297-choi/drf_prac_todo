425880398684dc3c16ff1ca3fb04fec17693717f
2017250297-choi <97ckdtn@gmail.com>
2023-04-30 18:07:46 +0900

    depart parsing payload and refresh

--- frontend/static/js/login.js
+++ frontend/static/js/login.js
@@ -1,11 +1,5 @@
-async function parse_payload(token) {
-    const base64Url = token.access.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    return jsonPayload
-}
+
+
 async function handlelogin() {
     const email = document.getElementById("email")
     const password = document.getElementById("password")
@@ -21,30 +15,13 @@ async function handlelogin() {
         })
     })
     const response_json = await response.json()
-    const jsonPayload = await parse_payload(response_json)
+    const jsonPayload = await parse_payload(response_json.access)
     localStorage.setItem("payload", jsonPayload);
     localStorage.setItem("refresh", response_json.refresh);
     localStorage.setItem("access", response_json.access);
 
 }
 
-
-async function refresh() {
-
-    const response = await fetch("http://127.0.0.1:8000/users/refresh/", {
-
-        method: "POST",
-        body: JSON.stringify({
-            "refresh": localStorage.getItem("refresh")
-        })
-    })
-    const response_json = await response.json()
-    console.log(response_json.access)
-    const jsonPayload = await parse_payload(response_json)
-    console.log(jsonPayload)
-    localStorage.setItem("payload", jsonPayload);
-    localStorage.setItem("access", response_json.access);
-}
 
 async function handlelogout() {
 

8afd735c46702e03458e9f0839cf1a6e571c995b
2017250297-choi <97ckdtn@gmail.com>
2023-04-30 18:05:39 +0900

    front-delete account

--- frontend/static/js/signup.js
+++ frontend/static/js/signup.js
@@ -1,3 +1,5 @@
+
+
 async function handleSignin() {
     const email = document.getElementById("email").value
     const password = document.getElementById("password").value
@@ -16,14 +18,34 @@ async function handleSignin() {
         "gender": gender,
         "introduction": introduction
     }
+    let headerdata = { "content-type": "application/json" }
+    const access_token = await get_access_token()
+    if (access_token != null) {
+        headerdata['authorization'] = access_token
+    }
 
     const response = await fetch("http://127.0.0.1:8000/user/", {
-        headers: {
-            "content-type": "application/json",
-        },
+        headers: headerdata,
         method: "POST",
         body: JSON.stringify(request_body)
     })
+    const result = await response.json()
+    console.log(result)
+}
+async function handleUserDel() {
+    let headerdata = {}
+    const access_token = await get_access_token()
+    if (access_token != null) {
+        headerdata['authorization'] = access_token
+    }
 
-
+    const response = await fetch("http://127.0.0.1:8000/user/", {
+        headers: headerdata,
+        method: "DELETE",
+    })
+    const result = await response.json()
+    console.log(result)
+    localStorage.removeItem('payload')
+    localStorage.removeItem('access')
+    localStorage.removeItem('refresh')
 }
